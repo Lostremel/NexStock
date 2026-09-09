a5ba3ba07b75fffee0606a24bdd604217ef5c0f9
Leonardo Stremel <141530013+Lostremel@users.noreply.github.com>
2026-09-09 11:13:24 -0300

    Delete nexstock directory

--- nexstock/js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAfdVpQjYxyF6gjj2ebmj8Fit1sNkG-Ux8",
-  authDomain: "isabela-semijoias.firebaseapp.com",
-  projectId: "isabela-semijoias",
-  storageBucket: "isabela-semijoias.firebasestorage.app",
-  messagingSenderId: "941718108706",
-  appId: "1:941718108706:web:ce3f4237af35ea0568e87b"
-};
